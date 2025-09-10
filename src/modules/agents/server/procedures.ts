ba692c58e4c62ec34b217fb47317b8159ee30513
moavia <mmoavia2002@gmail.com>
2025-09-10 20:06:00 +0500

    feat(agents): add empty state component and integrate with agents view

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -3,14 +3,18 @@ import { agents } from "@/db/schema";
 import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInsertSchema } from "../schema";
 import z from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
     .query(async ({ input }) => {
       const [existingAgent] = await db
-        .select()
+        .select({
+          // TODO: Replace with actual meeting count
+          meetingCount: sql<number>`5`,
+          ...getTableColumns(agents),
+        })
         .from(agents)
         .where(eq(agents.id, input.id))
         .limit(1);
@@ -18,7 +22,13 @@ export const agentsRouter = createTRPCRouter({
       return existingAgent;
     }),
   getMany: protectedProcedure.query(async () => {
-    const data = await db.select().from(agents);
+    const data = await db
+      .select({
+        // TODO: Replace with actual meeting count
+        meetingCount: sql<number>`5`,
+        ...getTableColumns(agents),
+      })
+      .from(agents);
 
     return data;
   }),
